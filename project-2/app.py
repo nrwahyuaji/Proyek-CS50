d2420b574d4acec7e3a46ee5b21fae9b9876d0ef
nrwahyuaji <nrwahyuaji@gmail.com>
2023-02-25 11:03:11 +0800

    Update: Home - Update app.py - Update styles.css (resize navbar, change menu properties) - Update layout.html

--- project-2/app.py
+++ project-2/app.py
@@ -1,15 +1,14 @@
 import os
 
-from flask import Flask, render_template, url_for, redirect, request
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup
-from cs50 import SQL
 
 # Configure application
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
