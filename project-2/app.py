9fb66d036c7f9129f0b24c2cc2acf54870d67e8e
nrwahyuaji <nrwahyuaji@gmail.com>
2023-02-25 20:15:56 +0800

    Update: App and DB - Add SQL Connection to database.db - Add database.db file

--- project-2/app.py
+++ project-2/app.py
@@ -10,6 +10,14 @@ from helpers import apology, login_required, lookup
 # Configure application
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///database.db")
+
 @app.route("/")
 def home():
     return render_template("home.html")
