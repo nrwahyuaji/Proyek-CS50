c323103a88951cb43bc9d7fb84f4bad9ec82df6b
nrwahyuaji <nrwahyuaji@gmail.com>
2023-02-25 10:44:19 +0800

    Update: - Re-routing app.py for home - Delete index.html - Edit layout.hml for main layout - Edit home.html to display halaman utama (to replace index.html)

--- project-2/app.py
+++ project-2/app.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
 
 @app.route("/math")
 def math():
